434cc51c8a7a0aa764d0b74bab9230e35233053a
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-04-16 00:58:22 +0530

    commit

---
+++ caress-frontend/pages/login.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+import React from 'react';
+
+export default function Login() {
+	return (
+		<>
+			<div className={styles.center}>
+			<img src="/caress-login-0.png" alt="caress" className='login-jpg' />
+        </div>
+		</>
+	)
+}
