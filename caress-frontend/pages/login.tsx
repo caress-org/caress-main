546f5c34868c8075315d43c5260178fdab07f495
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-04-16 15:37:40 +0530

    commit

--- caress-frontend/pages/login.tsx
+++ caress-frontend/pages/login.tsx
@@ -6,7 +6,22 @@ export default function Login() {
 		<>
 			<div className={styles.center}>
 			<img src="/caress-login-0.png" alt="caress" className='login-jpg' />
-        </div>
+		    </div>
+
+			<br />
+			<br />
+
+			<div className="login-btns">
+			<button className='login-btn'>
+				<div className='btn-column'><img className='login-logo' src="google-logo-9808.png" alt="" height={25}/><p>Continue with Google</p></div>
+			</button>
+			</div>
+
+			<div className="login-btns">
+			<button className='login-btn'>
+			<div className='btn-column'><img className='login-logo' src="facebook.png" alt="" height={25}/><p>Continue with Facebook</p></div>
+			</button>
+			</div>
 		</>
 	)
 }
