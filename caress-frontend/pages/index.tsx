0a42a24fbbbc401d254e0a24b78c69eaab87c6ba
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-04-18 18:11:10 +0530

    commit

--- caress-frontend/pages/index.tsx
+++ caress-frontend/pages/index.tsx
@@ -2,10 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+	
+	const router = useRouter();
+	
+	useEffect(() => {
+		router.push('/login');
+	  }, []);
+
   return (
     <>
       <Head>
@@ -15,7 +24,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-		
       </main>
     </>
   )
