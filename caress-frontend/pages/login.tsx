5081848820b404a78ae64d6f350fec4ff8c1ba30
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-04-18 23:25:37 +0530

    commit

--- caress-frontend/pages/login.tsx
+++ caress-frontend/pages/login.tsx
@@ -1,7 +1,29 @@
 import styles from '@/styles/Home.module.css'
+import firebase from '@/firebase/clientApp';
 import React from 'react';
+import 'firebase/compat/auth';
 
 export default function Login() {
+
+	const handleSignInWithFacebook = async () => {
+	  const auth = firebase.auth();
+	  const provider = new firebase.auth.FacebookAuthProvider();
+	  try {
+		await auth.signInWithPopup(provider);
+	  } catch (error) {
+		console.error(error);
+	  }
+	};
+
+	const handleSignInWithGoogle = async () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		try {
+		  await firebase.auth().signInWithPopup(provider);
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+
 	return (
 		<>
 			<div className={styles.center}>
@@ -12,13 +34,13 @@ export default function Login() {
 			<br />
 
 			<div className="login-btns">
-			<button className='login-btn'>
+			<button className='login-btn' onClick={handleSignInWithGoogle}>
 				<div className='btn-column'><img className='login-logo' src="google-logo-9808.png" alt="" height={25}/><p>Continue with Google</p></div>
 			</button>
 			</div>
 
 			<div className="login-btns">
-			<button className='login-btn'>
+			<button className='login-btn' onClick={handleSignInWithFacebook}>
 			<div className='btn-column'><img className='login-logo' src="facebook.png" alt="" height={25}/><p>Continue with Facebook</p></div>
 			</button>
 			</div>
