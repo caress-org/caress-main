2abd41a7ea57972a1ce92f146e07a43e33ca8921
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-04-24 04:13:32 +0530

    almost functional

---
+++ caress-frontend/pages/chats/chatbot.jsx
@@ -0,0 +1,112 @@
+import Head from 'next/head';
+import React, { useRef, useState, useEffect } from 'react';
+import styles from '@/styles/chats.module.css';
+import { LucideArrowLeft, LucideSend, LucideUser } from 'lucide-react';
+import { useRouter } from 'next/router';
+import firebase from '@/firebase/clientApp';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { Query } from '@firebase/firestore';
+import 'firebase/firestore';
+import auth from '@/firebase/detectSignin';
+
+export default function ChatBot() {
+
+	//interface User {
+	//	uid: string;
+	//	email: string | null;
+	//	displayName: string | null;
+	//	photoURL: string | null;
+	//	emailVerified: boolean;
+	//	phoneNumber: string | null;
+	//	isAnonymous: boolean;
+	//	tenantId: string | null;
+	//	providerData: any[];
+	//  }
+
+	//const [user, setUser] = useState("");
+
+	
+	const router = useRouter();
+
+	const goBack = () => {
+		router.replace("/chats");
+	}
+
+	const [user, setUser] = useState(null);
+  const [formValue, setFormValue] = useState('');
+  const dummy = useRef();
+	const firestore = firebase.firestore();
+  const messagesRef = firestore.collection('users').doc(user?.uid).collection('chatbot');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  useEffect(() => {
+		const authenticate = async () => {
+		const currentUser = await auth.isLoggedIn();
+		console.log('User object:', currentUser);
+		setUser(currentUser);
+    return currentUser;
+		}
+		authenticate();
+  }, []);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = user;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+
+
+	return (
+		<>
+		<Head>
+			<title>
+				ChatBot
+			</title>
+		</Head>
+		<div className={styles.headerr}>
+			<div className={styles.icon}>
+				<LucideArrowLeft onClick={goBack} className={styles.arrow} />
+				<div className={styles.chatAvatar}>
+					<LucideUser/>
+				</div>
+				<p className={styles.name}>ChatBot</p>
+			</div>
+		</div>
+		{messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+<span ref={dummy}></span>
+		<form className={styles.form} onSubmit={sendMessage}>
+
+      <input className={styles.input} value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Message..." />
+
+      <button className={styles.button} type="submit" disabled={!formValue}><LucideSend/></button>
+
+    </form>
+		</>
+	)
+}
+
+function ChatMessage(props) {
+	const { text, uid } = props.message;
+
+	//const messageClass = uid === user.uid ? 'sent' : 'received';
+
+	return (<>
+		<div className={styles.message}>
+			{/*<img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />*/}
+			<p className={styles.text}>{text}</p>
+		</div>
+	</>)
+}
